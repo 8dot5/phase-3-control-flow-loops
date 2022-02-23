d2f1896535d74f74b16fe08ce5e8c42d49271746
Chun Yi <8.5by11@gmail.com>
2022-02-23 11:52:30 -0500

    commit tree e9ae50d72c968791e9a719c8f5224130f950b6c1

--- looping.rb
+++ looping.rb
@@ -1,6 +1,12 @@
 def happy_new_year
-  # your code here
+  counter = 10
+  while counter > 0
+    puts counter
+    counter -= 1
+  end
+    puts "Happy New Year!"
 end
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -16,9 +22,50 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  (1..100).each do |num|
+    if num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+  end
 end
 
+# def reverse_string(str)
+#   string = ""
+#   str.size.times do |i| 
+#     string << str[-1 * i -1]
+#   end
+#   return string
+# end
+# reverse_string("hello")
+
+
+# with each pass through the loop, each letter from ‘string’ is added to the BEGINNING of ‘rev_string’. rev_string = str[i] + rev_string
 def reverse_string(str)
-  # your code here
+  rev_string = ""
+  i = 0
+  while i < str.length
+    rev_string = str[i] + rev_string
+    i += 1
+  end
+  return rev_string
 end
+
+
+
+#times loop method
+# 10.times do |i|
+#   puts "i is: #{i}"
+# end
+# OR
+#10.times { |i| puts "i is: #{i}"}
+
+# each loop method using range
+# (1..20).each do |num|
+#   puts num
+# end
